79ac69063f10e3e0310baa5f6609ca7fd103c62a
Edward Paler <paler.edward@gmail.com>
2021-10-28 16:14:10 +0800

    Complete code database

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3001;
 
 //Connection to monoDB Atlas
 //Syntax: mongoose.connect("<MongoDB Atlas connection string>", {userNewUrlParser:true});
-mongoose.connect("mongodb+srv://dbedwardpaler:A9oJgn0nL4BAbIcf@wdc028-course-booking.tgio6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://dbedwardpaler:A9oJgn0nL4BAbIcf@wdc028-course-booking.tgio6.mongodb.net/b138_to-do?retryWrites=true&w=majority",
 	{
 		useNewUrlParser:true,
 		useUnifiedTopology:true	
@@ -24,9 +24,62 @@ db.on("error", console.error.bind(console, "connection error"));
 //If the connection is successful, output in the console
 db.once("open", () => console.log("We're connected to the cloud database"));
 
+//Schema - determines the structure of the documents to be written in the database
+const taskSchema = new mongoose.Schema({
+	name: String,
+	status: {
+		type: String,
+		default: "pending"
+	}
+})
+
+//Create the Task model
+
+const Task = mongoose.model("Task", taskSchema);
 
 app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
+
+
+//Create a POST route to create a new task
+app.post("/tasks", (req, res) => {
+	Task.findOne({name: req.body.name}, (err, result) => {
+		if(result != null && result.name == req.body.name){
+			return res.send("Duplicate task found");
+		}
+		else{
+			let newTask = new Task({
+				name: req.body.name
+			})
+
+			newTask.save((saveErr, savedTask) => {
+				if(saveErr){
+					return console.error(saveErr);
+				}
+				else{
+					return res.status(201).send("New task created")
+				}
+			})
+		}
+	})
+})
+
+
+//Create a GET request to retrieve all the tasks
+app.get("/tasks", (req, res) => {
+	Task.find({}, (err, result) => {
+		//if an error occured
+		if(err){
+			return console.log(err);
+		}
+		//if no errors are found
+		else{
+			return res.status(200).json({
+				data: result
+			})
+		}
+	})
+})
 
 app.listen(port, () => console.log(`Server is running at port ${port}`));
